c443f0961296ef0c01dec8110e3683ee1d041739
Joechen0130 <piercejoe0130@gmail.com>
2025-05-02 15:26:51 +0800

    Implement Stack using Array

---
+++ Stack/Stack_array.c
@@ -0,0 +1,56 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_SIZE 5
+
+typedef struct{
+    int top;
+    int a[MAX_SIZE];
+}stack;
+
+bool isFull(stack* s){
+    return (s->top < MAX_SIZE)?false:true;
+}
+bool isEmpty(stack* s){
+    return (s->top ==0)?true:false;
+}
+void push(stack *s, int value) {
+    if (isFull(s)) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    s->a[s->top++] = value;
+}
+int pop(stack* s){
+    if(isEmpty(s)){
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    else{
+        s->top--;
+        return s->a[s->top];
+    }
+}
+void Print(stack* s){
+    printf("Top ");
+    for(int i=0;i<s->top;i++){
+        printf("%d->",s->a[i]);
+    }
+    printf(" bottom\n");
+}
+int main() {
+    stack* s = (stack*)malloc(sizeof(stack));
+    //init.
+    s->top = 0;
+    for(int i=0;i<10;i++){
+        push(s,i);
+        Print(s);
+    }
+    for(int i=0;i<10;i++){
+        printf("Pop:%d\n",pop(s));
+        Print(s);
+    }
+    return 0;
+}
